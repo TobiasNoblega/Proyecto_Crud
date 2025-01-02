4b53eef352f937435970c8757839cac986221c03
natalia <nataliagonzalezuba@gmail.com>
2025-01-02 18:29:44 -0300

    Descripción de los cambios realizados

--- src/servidor/bbdd.js
+++ src/servidor/bbdd.js
@@ -1,19 +1,16 @@
-const mysql = require('mysql2');
-require('dotenv').config({path: './env/.env'});
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const conexion = mysql.createConnection({
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
-});
+// URI de conexión
+const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/proyectoReactBD';
 
-conexion.connect((err)=>{
-    if(err){
-        throw err;
-    }else{
-        console.log("conexion a la bbdd exitosa")
-    }
-});
-
-module.exports = conexion;
+// Conexión con la base de datos
+mongoose.connect(uri)
+  .then(() => {
+    console.log('Conexión exitosa a MongoDB');
+    process.exit(0); // Cierra el proceso de Node.js al confirmar la conexión
+  })
+  .catch(err => {
+    console.error('Error al conectar a MongoDB:', err);
+    process.exit(1); // Cierra el proceso con un código de error
+  });
